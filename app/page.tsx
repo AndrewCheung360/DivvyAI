28fb97505843fe851804f6169068dfb625e50f5c
Andrew Cheung <andrewcheung360@gmail.com>
2024-10-05 03:38:41 -0400

    finished sign in page and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-
+import SignInForm from "@/components/auth/SignInForm";
+import { Sign } from "crypto";
 
 export default function Index() {
   return (
     <>
-    <div>Hello World</div>
+    <div className = "w-[100vw] bg-[#F4f4f4] h-[100vh] flex justify-center items-center">
+      <SignInForm />
+    </div>
     </>
   );
 }
