af2fc34bf6753239aa3ec7e84197b60cb6fa7b0a
James Tu <jamestvvo@gmail.com>
2024-10-05 22:27:47 -0400

    started inputcomponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useState } from "react";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import SignInForm from "@/components/auth/SignInForm";
 import AddButton from "@/components/home/AddButton";
@@ -11,8 +12,16 @@ import SelectEstimatedTime from "@/components/estimated-time/EstimatedTime";
 import PrioritySelector from "@/components/choose-priority/ChoosePriority";
 import { UploadAssignment } from "@/components/upload-assignment/UploadAssignment";
 import UploadReference from "@/components/upload-reference/UploadReference";
+import FormInput from "@/components/form-input/FormInput";
+
+
+import Clipboard from "/public/clipboard.svg";
+import Whiteboard from "/public/whiteboard.svg";
+
 
 export default function Index() {
+  const [assignment, setAssignment] = useState<string>("");
+  const [corseName, setCourseName] = useState<string>("");
   return (
     <>
     <div className = "w-[100vw] bg-[#F4f4f4] h-[150vh] flex justify-center items-center">
@@ -24,8 +33,24 @@ export default function Index() {
       {/* <DeadlineButton/> */}
       {/* <SelectEstimatedTime/> */}
       {/* <PrioritySelector/> */}
-      <UploadAssignment/>
+      {/* <UploadAssignment/> */}
       {/* <UploadReference/> */}
+      <FormInput 
+        icon={Clipboard} 
+        type={"text"}
+        alt={"Clipboard Icon"} 
+        placeholder={"Name of the assignment"} 
+        setValue={setAssignment}
+        />
+      <FormInput 
+        icon={Whiteboard}
+        type={"text"}
+        alt={"Whiteboard Icon"} 
+        placeholder={"Name of the course"}
+        setValue = {setCourseName}
+        />
+      {assignment}
+      {corseName}
     </div>
     </>
   );
