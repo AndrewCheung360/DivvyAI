85a89fb5f4176f5cfd5dc46b2430ef90f5b67d23
Andrew Cheung <andrewcheung360@gmail.com>
2024-10-06 03:16:31 -0400

    Implemented SideBar component:

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import SignInForm from "@/components/auth/SignInForm";
 import AddButton from "@/components/home/AddButton";
 import ImportScheduleButton from "@/components/home/ImportScheduleButton";
+import SideBar from "@/components/home/SideBar";
 
 export default function Index() {
   return (
     <>
     <div className = "w-[100vw] bg-[#F4f4f4] h-[100vh] flex justify-center items-center">
-      <ImportScheduleButton/>
-      <AddButton/>
+      <SideBar/>
     </div>
     </>
   );
