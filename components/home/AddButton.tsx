9a458487165ae5fd72b31909dde3e448b5610cd4
JasonMun7 <jasonmun020104@gmail.com>
2024-10-06 05:07:28 -0400

    Major Front end fixes and finalized layout

--- components/home/AddButton.tsx
+++ components/home/AddButton.tsx
@@ -3,7 +3,7 @@ import React from "react"
 
 export default function AddButton(){
     return (
-        <button className = "w-[262px] h-[66px] bg-[#141414] rounded-[50px] flex justify-center items-center border-2 border-[#141414] gap-x-3 hover:bg-[#2b2b2b] cursor-pointer">
+        <button className = "w-[262px] h-[66px] bg-[#141414] rounded-[50px] flex justify-center items-center border-2 border-[#141414] gap-x-3 transition-transform duration-200 hover:scale-110 hover:cursor-pointer">
             <span className = "text-white text-2xl font-light">
                 +
             </span>
